669872857e4077da18ce2f290781e4e3d08373ee
TatianaIaquinto <tati.iaquinto@gmail.com>
2024-11-07 08:57:18 -0300

    page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,85 @@
+import { Gift } from "lucide-react";
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const giftsItems = [
+    {
+      image:"/img/cansada.png",
+      href:"https://www.icebrg.com.br/bateria",
+      title:"Exausta"
+    },
+    {
+      image:"/img/cinefila.png",
+      href:"https://moonpix.com.br/paul-mescal-7mcbo",
+      title:"Amante da sétima arte"
+    },
+    {
+      image:"/img/aventureira.png",
+      href:"https://a.co/d/30Dl82d",
+      title:"Aventureira"
+    },
+    {
+      image: "/img/marujo.png",
+      href:"https://funniest.com.br/produtos/camiseta-lana-del-rey-mariners-apartment-complex/",
+      title:"Desbravadoura dos sete mares"
+    },
+    {
+      image: "/img/lagoa.png",
+      href:"https://a.co/d/1dooKde",
+      title:"Grande fã de grandes corpos de água"
+    },
+    {
+      image: "/img/tatuada.png",
+      href:"https://riotdesign.com.br/brinco-stud-andorinhas-old-school-prateado-660",
+      title:"Tatuada, descolada, old school"
+    }
+  ]
+
+  return (
+    <>
+      <header className="w-full h-14 bg-[#9b5de5] p-5">
+       <Gift className="text-white"/>
+      </header>
+
+      <main className="w-full h-full bg-[#F15BB5] text-white pb-11">
+      <section className="flex flex-col items-center pt-9">
+        <div className="px-6 text-center">
+          <h1 className="font-bold text-3xl">
+            Que sorte a sua em me ter (rs) como sua amiga secreta!
+          </h1>
+          <h3 className="my-6">Um presente para cada Tatiana. Clique na foto e descubra:</h3>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      
+        <Carousel className="w-[200px] md:w-[400px] lg:w-[600px]">
+        <CarouselContent >
+        {giftsItems.map((item, index) => (
+          <CarouselItem key={index}>
+            <Card>
+              <CardContent className="pt-6">
+              <a href={item.href} target="_blank" >
+                <Image src={item.image} alt="" width={600} height={600}/>
+              </a>
+              
+              </CardContent>
+              <CardFooter className="w-full flex justify-center text-xl"><p>{item.title}</p></CardFooter>
+            </Card>
+          </CarouselItem>
+        )) }
+        </CarouselContent>
+        <CarouselNext className="bg-transparent"/>
+        <CarouselPrevious className="bg-transparent"/>
+        </Carousel>
+      </section>
+      <footer className="text-center mt-12">
+        Obs: se algum item está mais caro do que deveria ou é porque estava em alguma promo e acabou OU é pq eu sou uma sacana mesmo.
       </footer>
-    </div>
+
+      </main>
+    
+      
+    </>
   );
 }
